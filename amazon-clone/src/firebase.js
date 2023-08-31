d088a8c6008b51b049ac5004f4325e50d42a37b2
vishilp <69099134+vishilp@users.noreply.github.com>
2023-08-30 22:22:24 -0700

    Delete amazon-clone directory

--- amazon-clone/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyA4d_BtgOM4Sr-EKF7KT0uE6rHQ-bj9O4E",
-    authDomain: "clone-b7552.firebaseapp.com",
-    projectId: "clone-b7552",
-    storageBucket: "clone-b7552.appspot.com",
-    messagingSenderId: "689090585585",
-    appId: "1:689090585585:web:0e5ee774edbc3b776e9a50"
-  };
